e86f3165b6fe7a1433299c3a81565e21887e109e
Linar <latypov.ld@yandex.ru>
2023-02-05 16:48:41 +0500

    add stats client

--- stats-service/stats-server/src/main/java/ru/practicum/controller/StatsController.java
+++ stats-service/stats-server/src/main/java/ru/practicum/controller/StatsController.java
@@ -21,20 +21,22 @@ import java.util.Set;
 public class StatsController {
     private final StatsService statsService;
 
+    private static final String DTFPattern = "yyyy-MM-dd HH:mm:ss";
+
     @PostMapping("/hit")
     public ResponseEntity<Void> createHit(@RequestBody @Valid EndpointHitDto endpointHitDto) {
-        log.info("Hit with URI = {} save to statistic", endpointHitDto.getUri());
+        log.info("Hit with URI = {} save to stats", endpointHitDto.getUri());
         statsService.saveEndpointHit(endpointHitDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping("/stats")
     public ResponseEntity<List<ViewStatsDto>> getStats(
-            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
-            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
+            @RequestParam @DateTimeFormat(pattern = DTFPattern) LocalDateTime start,
+            @RequestParam @DateTimeFormat(pattern = DTFPattern) LocalDateTime end,
             @RequestParam(required = false) Set<String> uris,
             @RequestParam(required = false, defaultValue = "false") boolean unique) {
-        log.info("Get hits statistics ");
+        log.info("Get hits statistics");
         return ResponseEntity.status(HttpStatus.OK).body(statsService.getStats(start, end, uris, unique));
     }
 }
