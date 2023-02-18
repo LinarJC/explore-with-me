d5038b1332de8927fd4dffd51b82a952e25a3803
Linar <latypov.ld@yandex.ru>
2023-02-19 00:07:53 +0500

    fix for PR

--- ewm-service/src/main/java/ru/practicum/ewmsvc/category/controller/CategoryController.java
+++ ewm-service/src/main/java/ru/practicum/ewmsvc/category/controller/CategoryController.java
@@ -2,7 +2,10 @@ package ru.practicum.ewmsvc.category.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import ru.practicum.ewmsvc.category.repository.CategoryRepository;
 import ru.practicum.ewmsvc.category.service.CategoryService;
 import ru.practicum.ewmsvc.category.dto.CategoryDto;
 
@@ -13,6 +16,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping(path = "/categories")
 public class CategoryController {
+    private final CategoryRepository categoryRepository;
     private final CategoryService categoryService;
 
     @GetMapping
@@ -27,6 +31,9 @@ public class CategoryController {
     @GetMapping("/{catId}")
     public CategoryDto getCategory(@PathVariable Long catId) {
         log.info("Request endpoint: 'GET /categories/{}", catId);
+        if(!categoryRepository.existsById(catId)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No such category");
+        }
         return categoryService.getCategory(catId);
     }
 }
